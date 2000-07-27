4144e23fe9770ee9d33c4d39832c22be661e5fec
Dave Cross <dave@perlhacks.com>
2000-07-27 10:22:37 +0000

    Import of DAVECROSS/Sub-Approx-0.02 from CPAN.
    gitpan-cpan-distribution: Sub-Approx
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         DAVECROSS/Sub-Approx-0.02.tar.gz
    gitpan-cpan-author:       DAVECROSS
    gitpan-cpan-maturity:     released

--- Approx.pm
+++ Approx.pm
@@ -4,7 +4,7 @@ use strict;
 use vars qw($VERSION @ISA $AUTOLOAD);
 use Text::Soundex;
 
-$VERSION = '0.01';
+$VERSION = '0.02';
 
 # import is called when another script uses this module.
 # All we do here is overwrite the callers AUTOLOAD function
